fb1d8e2abeb7495725670a66fe62368dfe61ebcb
Vinicius <vinicius.freitas@hsl.org.br>
2024-09-19 15:59:26 -0300

    feat: Adding Swipe on the Product Carousel

--- src/components/ProductCarousel/index.tsx
+++ src/components/ProductCarousel/index.tsx
@@ -1,17 +1,30 @@
-import { Wrapper } from "./styles";
+import { ProductCardContainer, Wrapper } from "./styles";
 import products from "@mocks/productsBlockContent.json";
 import { ProductCard } from "@components/ProductCard";
 
+import { Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
 export function ProductCarousel() {
   return (
-    <Wrapper>
-      {products.map(({ title, image, price, oldPrice }) => (
-        <ProductCard
-          title={title}
-          image={image}
-          price={price}
-          oldPrice={oldPrice}
-        />
+    <Wrapper
+      slidesPerView={5}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+    >
+      {products.map(({ title, image, price, oldPrice }, index) => (
+        <ProductCardContainer key={index}>
+          <ProductCard
+            title={title}
+            image={image}
+            price={price}
+            oldPrice={oldPrice}
+          />
+        </ProductCardContainer>
       ))}
     </Wrapper>
   );
--- src/components/ProductCarousel/styles.ts
+++ src/components/ProductCarousel/styles.ts
@@ -1,7 +1,14 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+import { Swiper, SwiperSlide } from "swiper/react";
+
+export const Wrapper = styled(Swiper)`
   display: flex;
   flex-direction: row;
   gap: 16px;
+  width: 100%;
+  height: 100%;
+  padding-bottom: 45px;
 `;
+
+export const ProductCardContainer = styled(SwiperSlide)``;
