7b4f9cd7608aabc1c5dd7c0340db0b13f53f8d14
Vinicius <vinicius.freitas@hsl.org.br>
2024-09-19 11:05:12 -0300

    feat: Adding a menu items on the Announcement Bar

--- src/components/AnnouncementBar/index.tsx
+++ src/components/AnnouncementBar/index.tsx
@@ -1,10 +1,32 @@
-import { Container, Wrapper } from "./styles";
+import { Container, MenuItem, MenuContainer, Wrapper } from "./styles";
+import { ReactSVG } from "react-svg";
+import LocationSVG from "../../assets/location.svg";
+import DeliveryTruckSVG from "../../assets/deliveryTruck.svg";
+import DiscountSVG from "../../assets/discount.svg";
 
 export function AnnouncementBar() {
   return (
     <Wrapper>
       <Container>
         <p>Welcome to worldwide Megamart!</p>
+        <MenuContainer>
+          <MenuItem>
+            <ReactSVG src={LocationSVG} />
+            <p>
+              Deliver to <strong>423651</strong>
+            </p>
+          </MenuItem>
+
+          <MenuItem>
+            <ReactSVG src={DeliveryTruckSVG} />
+            <p>Track your order</p>
+          </MenuItem>
+
+          <MenuItem>
+            <ReactSVG src={DiscountSVG} />
+            <p>All Offers</p>
+          </MenuItem>
+        </MenuContainer>
       </Container>
     </Wrapper>
   );
--- src/components/AnnouncementBar/styles.ts
+++ src/components/AnnouncementBar/styles.ts
@@ -11,6 +11,44 @@ export const Wrapper = styled.div`
 `;
 
 export const Container = styled.div`
+  display: flex;
   width: 100%;
   max-width: ${(props) => props.theme.boxedWidth};
+  justify-content: space-between;
+`;
+
+export const MenuContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const MenuItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  &::after {
+    content: "";
+    height: 18px;
+    width: 1px;
+    background-color: ${(props) => props.theme.line};
+    margin: 0px 16px;
+    border-radius: 8px;
+  }
+
+  &:last-child::after {
+    content: none;
+  }
+
+  p {
+    color: ${(props) => props.theme.text};
+    font-size: 14px;
+    padding-bottom: 3px;
+    margin-left: 6px;
+    font-weight: 300;
+    strong {
+      font-weight: 400;
+    }
+  }
 `;
