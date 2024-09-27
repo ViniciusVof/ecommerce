0c3df57e4dda210a5f74bc2efeefc80cdf5d17e5
Vinicius <vinicius.freitas@hsl.org.br>
2024-09-27 16:57:22 -0300

    feat: Adding links in menu header

--- src/components/MenuHeader/index.tsx
+++ src/components/MenuHeader/index.tsx
@@ -1,19 +1,24 @@
+import { useNavigate } from "react-router-dom";
 import { Container, MenuItem, Wrapper } from "./styles";
 
 interface IMenuItems {
   id: string | number;
   title: string;
+  slug: string;
 }
 
 interface IMenuHeader {
   menuItems: IMenuItems[];
 }
 export function MenuHeader({ menuItems }: IMenuHeader) {
+  const navigate = useNavigate();
   return (
     <Wrapper>
       <Container>
-        {menuItems.map(({ title, id }) => (
-          <MenuItem key={id}>{title}</MenuItem>
+        {menuItems.map(({ title, id, slug }) => (
+          <MenuItem onClick={() => navigate(`category/${slug}`)} key={id}>
+            {title}
+          </MenuItem>
         ))}
       </Container>
     </Wrapper>
